acbcc0207a26bb132970e5277bd37c3db4a8e66e
De Leon <julie.a.c.de.leon@accenture.com>
2021-07-29 16:44:59 +0800

    Initial commit

---
+++ public/js/app.js
@@ -0,0 +1,28 @@
+console.log("I'm now doing a html using javascripts ");
+
+const weatherForm = document.querySelector("form");
+const search = document.querySelector("input");
+const messageOne = document.querySelector("#message-1");
+const messageTwo = document.querySelector("#message-2");
+
+weatherForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  const location = search.value;
+
+  messageOne.textContent = "Loading weather forecast to your location.. ";
+  messageTwo.textContent = "";
+
+  fetch("http://localhost:3000/weather?address=" + location).then(
+    (response) => {
+      response.json().then((dataWeather) => {
+        if (data.error) {
+          messageOne.textContent = data.error;
+        } else {
+          messageOne.textContent = data.location;
+          messageTwo.textContent = data.forecast;
+        }
+      });
+    }
+  );
+});
---
+++ src/utils/forecast.js
@@ -0,0 +1,29 @@
+const request = require("request");
+
+const forecast = (latitude, longitude, callback) => {
+  const url =
+    "http://api.weatherstack.com/current?access_key=fd21031852b2152fffb70e3ad4c69720&query=" +
+    latitude +
+    "," +
+    longitude +
+    "&units=m";
+  request({ url, json: true }, (error, { body }) => {
+    if (error) {
+      callback("Unable to connect to weather services.", undefined);
+    } else if (body.error) {
+      callback("Unable to find location, try another search.", undefined);
+    } else {
+      callback(
+        undefined,
+        body.location.name +
+          ". It is currently " +
+          body.current.temperature +
+          " degrees out. There is a " +
+          body.current.precip +
+          "% chance of rain."
+      );
+    }
+  });
+};
+
+module.exports = forecast;
